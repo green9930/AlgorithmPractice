fdc2f0a6bb2141b4e6cd33f6676ce16e9101cf7b
GEUNA <geuna0204@gmail.com>
2021-01-10 16:35:01 +0900

    Even or Odd / completed

---
+++ 01-evenOrOdd.js
@@ -0,0 +1,18 @@
+// if문
+function evenOrOdd(num) {
+  if (num % 2 === 0) return 'Even';
+  return 'Odd';
+}
+
+console.log(evenOrOdd(2)); // Even
+console.log(evenOrOdd(3)); // Odd
+console.log(evenOrOdd(1000)); // Even
+
+// 삼항 조건 연산자
+function evenOrOdd(num) {
+  return num % 2 ? 'Odd' : 'Even';
+}
+
+console.log(evenOrOdd(2)); // Even
+console.log(evenOrOdd(3)); // Odd
+console.log(evenOrOdd(1000)); // Even
