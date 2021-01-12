e024caae10fa65cb5b381df8a5018ee687b7b618
GEUNA <geuna0204@gmail.com>
2021-01-12 23:27:47 +0900

    get MaxValue and Value from array / completed

---
+++ 10-getMinMaxValue.js
@@ -0,0 +1,14 @@
+// Get max value
+function getMaxValueFromArray(array) {
+  return Math.max(...array);
+}
+
+console.log(getMaxValueFromArray([3, 6, -2, -5, 7, 3])); // 7
+
+
+// Get min Value
+function getMinValueFromArray(array) {
+  return Math.min(...array);
+}
+
+console.log(getMinValueFromArray([3, 6, -2, -5, 7, 3])); // -5
